b3495aa83a81ce3db62ba431f50aca283c9c413d
logicmohe <46504798+logicmohe@users.noreply.github.com>
2020-11-30 18:36:45 -0400

    CPST Design

--- BLEmousewithIMU/BLEmousewithIMU.ino
+++ BLEmousewithIMU/BLEmousewithIMU.ino
@@ -1,6 +1,6 @@
 /*
-This is the program for Adafruit Feather M0 Bluefruit LE with Adafruit BNO08X IMU Module.
-In theory, it can be directly uses on Arduino M0 with Adafruit Bluefruit LE SPI Friend and Adafruit BNO08X IMU Module.
+  This is the program for Adafruit Feather M0 Bluefruit LE with Adafruit BNO085 IMU Module.
+  In theory, it can be directly uses on Arduino M0 with Adafruit Bluefruit LE SPI Friend and Adafruit BNO085 IMU Module.
 */
 
 //include libraries for microcontroller and BLE module
@@ -13,72 +13,40 @@ In theory, it can be directly uses on Arduino M0 with Adafruit Bluefruit LE SPI 
 
 //include libraries for IMU
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO08X.h>
-#include <utility/imumaths.h>
+#include <Adafruit_BNO08x.h>
 
-//Set IMU sample rate and rename the sensor in bno
-#define BNO08X_SAMPLERATE_DELAY_MS (10)
-Adafruit_BNO08X bno = Adafruit_BNO08X();
-
-//define all the pin numbers
-
-#define thumb A1
-#define index A2
-#define middle A3
-#define ring A4
-#define pinky A5
-
-#define ModeSW 12
-
-#define rgbRed 11
-#define rgbGreen 10
-#define rgbBlue 9
+//include other libraries
+#include "PinoutConfig.h"
+#include "peripherals.h"
+#include "FlexSensor.h"
+#include "GestureDefiniation.h"
 
 //settings for the project
-const int sensitivity = 22; //sensitivity of the movement
-const int debug = 1; //Debug flag
+const int sensitivity = 700; //sensitivity of the movement
+const int debug = 0; //Debug flag
+const int stream = 1; //3D rotational data streaming
 
-//initialize all global variables 
+//initialize all global variables
 int xMove, yMove, drift, b;
-float _x0, _y0;
-float _x1, _y1, _z1, _x2, _y2;
+bool leftClick = 0, rightClick = 0;
+float _x0, _y0, _z0;
+float _r1, _x1, _y1, _z1, _x2, _y2;
+
+
+// Set the delay between fresh samples
+Adafruit_BNO08x  bno08x(BNO08X_RESET);
+//declear variable for the sensor data
+sh2_SensorValue_t sensorValue;
 
 /*=========================================================================
-    APPLICATION SETTINGS
+  APPLICATION SETTINGS
 
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    -----------------------------------------------------------------------*/
-    #define FACTORYRESET_ENABLE         0
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+  FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+  MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+  -----------------------------------------------------------------------*/
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 /*=========================================================================*/
-
-
-// Create the bluefruit object, either software serial...uncomment these lines
-
 /* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
@@ -88,119 +56,49 @@ void error(const __FlashStringHelper*err) {
   while (1);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW and the BLE module (this function is called
-            automatically on startup)
-*/
-/**************************************************************************/
-
-int getBattery(void){
-//Convert battery voltage into 0~255 reading	
-	int value;
-	value = (analogRead(A7)*6.6/4096);
-	return map(value, 3.2, 4.2, 0, 255);
-}
-
-int LEDWrite(int R, int G){
-//lit RGB LED with Green and Red.	
-	analogWrite(rgbRed, R);
-	analogWrite(rgbGreen, G);
-}
-
-int FadeIn(int Pin){
-  //Lit cycle for RGB LED throught mixture of red, green and blue
-  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(Pin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+int cmove(int x, int y) {
+  //send move data of mouse curser to bluetooth module according to the given x,y value.
+  //send new value only when it's changed 
+  if((x != 0) or (y != 0)){
+    ble.print(F("AT+BleHidMouseMove="));
+    ble.print(x);
+    ble.print(",");
+    ble.println(y);
   }
   return 0;
 }
 
-int FadeOut(int Pin){
-//Off cycle for RGB LED throught mixture of red, green and blue
-  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
-    // sets the value (range from 0 to 255):
-    analogWrite(Pin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
-  }
-  return 0;    
-}
-
-int cmove(int x, int y){
-//send move data of mouse curser to bluetooth module according to the given x,y value.
-     ble.print(F("AT+BleHidMouseMove="));
-     ble.print(x);
-     ble.print(",");
-     ble.println(y);
-     return 0;
-}
-
-int fingerRead(int finger, int offset){
-//read one finger and give back a reading inbetween 0~4095
-  int reading;
-  reading = analogRead(finger)-offset;
-
-  return reading; //map(reading,0,4095,0,512);
-}
-
-int LeftClick(void){
-/*
-Read fingers and compare to the thresholds, adjust thresholds and fingers with debug flag enabled.
-Reading from finger is in between 0 to 4095.
-All five fingers are avaliable as input(thumb, index, middle, ring and pinky).
-*/
-  int data = 0;
-  for( int i=0; i <= 10; i++){
-    if(fingerRead(middle, 0)> 2000 && fingerRead(ring, 0)> 2300){
-	data += 1;
-     } else {
-	data += 0;
-     }
-  }
-  /*
-  to filter the noise, the following lines are averaging how many reading is higher
-  than the thresholds in given numbers of continuous samples to be considered as a valid 
-  click. (in this case 85% reading has to be higher than thresholds in 10 continuous 
-  samples)  
-  */
-  if(data/10 >= 0.85){
-	return 1;
-  } else {
-	return 0;
+//Define the BNO080 sensor outputs format
+void setReports(void) {
+  Serial.println("Setting desired reports");
+  if (! bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+    Serial.println("Could not enable game vector");
   }
 }
 
-void setup(void){
-
+void setup(void) {
+  /***********Configure the microcontroller***********/
   while (!Serial and debug);  // required for Flora & Micro
   delay(500);
 
   Serial.begin(115200);
 
-  pinMode(ModeSW,INPUT);
+  pinMode(ModeSW, INPUT);
+  analogReadResolution(12); //Change the ADC resolution to 12 bits
   
-  ADC->INPUTCTRL.bit.GAIN = ADC_INPUTCTRL_GAIN_2X_Val; //Set internal gain
-  ADC->REFCTRL.bit.REFSEL = ADC_REFCTRL_REFSEL_INTVCC0_Val;
-  //ADC->AVGCTRL.bit.SAMPLENUM = ADC_AVGCTRL_SAMPLENUM_8_Val; // average from 4 samples
-  analogReadResolution(12); //Change the ADC resolution to 12 bits 
-
-  //initialize IMU module
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO08X ... check your connections */
-    Serial.print("Ooops, no BNO08X detected ... Check your wiring or I2C ADDR!");
-    while(1);
+  /****************Configure the IMU****************/
+  // Try to initialize BNO080 IMU!
+  if (!bno08x.begin_I2C(0x4B)) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) { delay(10); }
   }
-  bno.setExtCrystalUse(true); //set IMU module to use external crystal 
-
-  //Initialise the BLU module
+  Serial.println("BNO08x Found!");
+  setReports(); //set pre-defined report data for BNO085
+  Serial.println("BNO08x Report Format Set!");
+  
+  /****************Configure the BLU****************/
   Serial.print(F("Initialising the Bluefruit LE module: "));
-
-  if ( !ble.begin(VERBOSE_MODE) ) //
+  if ( !ble.begin(VERBOSE_MODE) )
   {
     error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
   }
@@ -208,39 +106,37 @@ void setup(void){
 
   if ( FACTORYRESET_ENABLE )
   {
-    /* Perform a factory reset to make sure everything is in a known state */
+    //Perform a factory reset to make sure everything is in a known state
     Serial.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
+    if ( ! ble.factoryReset() ) {
       error(F("Couldn't factory reset"));
     }
   }
 
-  /* Disable command echo from Bluefruit */
+  //Disable command echo from Bluefruit
   ble.echo(false);
 
   Serial.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
+  //Print Bluefruit information
   ble.info();
 
-  // This demo only available for firmware from 0.6.6
+  // The mouse function requires a minimium firmware version of0.6.6
   if ( !ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
   {
     error(F("This sketch requires firmware version " MINIMUM_FIRMWARE_VERSION " or higher!"));
   }
-
   /* Enable HID Service (including Mouse) */
   Serial.println(F("Enable HID Service (including Mouse): "));
   if (! ble.sendCommandCheckOK(F( "AT+BleHIDEn=On"  ))) {
     error(F("Failed to enable HID (firmware >=0.6.6?)"));
   }
 
-  /* Add or remove service requires a reset */
-  Serial.println(F("Performing a SW reset (service changes require a reset): "));
+  //Add or remove service requires a reset
+  Serial.println(F("Performing a SW reset"));
   if (! ble.reset() ) {
     error(F("Could not reset??"));
   }
   Serial.println();
-    
 }
 
 /**************************************************************************/
@@ -248,99 +144,105 @@ void setup(void){
     @brief  Constantly poll for new command or response data
 */
 /**************************************************************************/
-void loop(void){
-//Two main loops for enable and disable function	
-  while(digitalRead(ModeSW) == 1){
-
-    imu::Vector<3> euler = bno.getVector(Adafruit_BNO08X::VECTOR_EULER); //get euler angle from IMU
-
-    _x1 = (euler.x());
-    _y1 = (euler.y());
-    _z1 = (euler.z());
-
-    _y2 = _y1- _y0;  //compare the current reading to previous reading to see how far hand was moved
-    _x2 = _x1- _x0;
-    _y0 = _y1;  //Store current readings for next cycle
-    _x0 = _x1; 
-	
-    if (_y2 != 0 or _x2 != 0){
-	xMove = _x2 * sensitivity * 1.5;  //screen has greater length than width, times 1.5
-        yMove = -(_y2) * sensitivity * 1;
-	//remove slight drifting when hand is not moving.    
-	if(yMove > 0){
-		drift = 1;
-	} else if(yMove < 0){
-		drift = -1;
-	} else {drift = 0;}
-		
-	if(xMove > 0){
-		drift = 1;
-	} else if(xMove < 0){
-		drift = -1;
-	} else {drift = 0;}
-		
-        cmove(xMove-drift, yMove-drift); // send data and move mouse
-	}
-	
-    /******************************************************/
-    /*if (_y2 != 0){
-        yMove = -(_y2) * sensitivity * 1.8;
-		if(yMove > 0){
-			drift = 1;
-		} else if(yMove < 0){
-			drift = -1;
-		} else {drift = 0;}
-		
-        cmove(0, yMove-drift); // move mouse on y axis
-	}
-	  
-    if (_x2 != 0){
-        xMove = _x2 * sensitivity;
-		if(xMove > 0){
-			drift = 1;
-		} else if(xMove < 0){
-			drift = -1;
-		} else {drift = 0;}
-		cmove(xMove-drift, 0); // move mouse on x axis
-	}*/
-	/******************************************************/
-    
-    if(LeftClick() == 1){
-      ble.sendCommandCheckOK(F("AT+BleHidMouseButton=L,press"));
+void loop(void) {
+  //For enable and disable function
+  while (digitalRead(ModeSW) == 1) {
+    //request sensor data from BNO08X
+    if (! bno08x.getSensorEvent(&sensorValue)) {
+      return;
     }
-    if(LeftClick() == 0){
+    //map the quaternions readings to 2d movement
+    _r1 = sensorValue.un.gameRotationVector.real;
+    _x1 = sensorValue.un.gameRotationVector.k;
+    _y1 = sensorValue.un.gameRotationVector.i;
+    _z1 = sensorValue.un.gameRotationVector.j;
+    //only 2 ratation axis are required for mouse demonstration
+    _y2 = _y1 - _y0; //compare the current reading to previous reading to see how far hand was moved
+    _x2 = _x1 - _x0;
+    _y0 = _y1;  //Store current readings for next cycle
+    _x0 = _x1;
+
+    if (_y2 != 0 or _x2 != 0) {
+      xMove = -(_x2) * sensitivity * 1.5;  //screen has greater length than width, times 1.5(ratio of the screen)
+      yMove = -(_y2) * sensitivity * 1;
+      //remove slight drifting when hand is not moving.
+      if (yMove > 0) {
+        drift = 1;
+      } else if (yMove < 0) {
+        drift = -1;
+      } else {
+        drift = 0;
+      }
+
+      if (xMove > 0) {
+        drift = 1;
+      } else if (xMove < 0) {
+        drift = -1;
+      } else {
+        drift = 0;
+      }
+      cmove(xMove - drift, yMove - drift); // send data and move mouse
+    }
+    
+    if ((LeftClick() == 1) && (leftClick == 0)) {
+      leftClick = 1;
+      ble.sendCommandCheckOK(F("AT+BleHidMouseButton=L,press"));
+      
+    }
+    if ((LeftClick() == 0) && (leftClick == 1)) {
+      leftClick = 0;
       ble.sendCommandCheckOK(F("AT+BleHidMouseButton=0"));
     }
-	
-    if(debug){
-    //print real time reading to serial port(may lagging the movement)
-	Serial.print(xMove-drift);
-	Serial.print(",");
-	Serial.print(yMove-drift);
-	Serial.print(",");
-	Serial.print(fingerRead(index, 0));
-	Serial.print(",");
-	Serial.print(fingerRead(middle, 0));
-	Serial.print(",");
-	Serial.print(fingerRead(ring, 0));
-	Serial.print(",");
-	Serial.println(LeftClick());
-    }
-  }  
-  //second main loop
-  while(digitalRead(ModeSW) == 0){
-	FadeIn(rgbRed);
-	delay(1000);
-	FadeIn(rgbGreen);
-	delay(1000);
-	FadeIn(rgbBlue);
-	delay(1000);
 
-	FadeOut(rgbRed);
-	delay(1000);
-	FadeOut(rgbGreen);
-	delay(1000);
-	FadeOut(rgbBlue);
-	delay(1000);
+    if (debug) {
+      //print debug readings
+      Serial.print(xMove - drift);
+      Serial.print(",");
+      Serial.print(yMove - drift);
+      Serial.print(",");
+      Serial.print(fingerRead(index, 0));
+      Serial.print(",");
+      Serial.print(fingerRead(middle, 0));
+      Serial.print(",");
+      Serial.print(fingerRead(ring, 0));
+      Serial.print(",");
+      Serial.println(LeftClick());
+    }
+    if (stream) {
+      //print real time IMU and flex sensor readings to serial port
+      Serial.print(_r1);
+      Serial.print(",");
+      Serial.print(_y1);
+      Serial.print(",");
+      Serial.print(_z1);
+      Serial.print(",");
+      Serial.print(_x1);
+      Serial.print(",");
+      Serial.print(fingerRead(index, 0));
+      Serial.print(",");
+      Serial.print(fingerRead(middle, 0));
+      Serial.print(",");
+      Serial.print(fingerRead(ring, 0));
+      Serial.print(",");
+      Serial.println(LeftClick());
+    }
+
+    delay(25);
+  }
+  //second loop
+  while (digitalRead(ModeSW) == 0) {
+    FadeIn(rgbRed);
+    delay(1000);
+    FadeIn(rgbGreen);
+    delay(1000);
+    FadeIn(rgbBlue);
+    delay(1000);
+
+    FadeOut(rgbRed);
+    delay(1000);
+    FadeOut(rgbGreen);
+    delay(1000);
+    FadeOut(rgbBlue);
+    delay(1000);
   }
 }
--- README.md
+++ README.md
@@ -1,44 +1,6 @@
 # Project-Glove<br />
-This project is to create a hand glove that capture motions to control a computer or other
-devices. It will increase capacity of control in VR and other computer systems. The range of
-motion will change from pushing buttons and triggers to active movements of the hands.
-
-This is a 2nd Year Engineeing Design II Group Project at Acadia University.<br />
-
-**Group 12**<br />
-
-Omar H. Khattab/ Project Manager<br />
-Hannah MacCormnack/ Project Researcher<br />
-Haolin Song/ Project Designer<br />
-Siyan Zhang/ Project Technican<br />
-
-**Current State**<br />
-The basic function of mouse is working, but due to the limited performance and pooly optimized code, add any additional function will causes signifecant delay of the motion.
-
-**Todo List**<br />
-1. Simplify the code to make code more efficiency.
-2. Waiting for **New** ATSAMD51 series with hardware DSP and Float Point support.
+This project is to create a hand glove that capture motions to control a computer or other devices. It will increase capacity of control in VR and other computer systems. The range of motion will change from pushing buttons and triggers to active movements of the hands.
 
 
-![Image of Glove](https://github.com/zhangsiyan12134/Project-Glove/raw/master/29748747_10155416341501417_397483712_o.jpg)
 
-Demo Video：https://youtu.be/c8829fM5WGY
-
-
-**Parts List**<br />
-
-Part     Value          Device             Package         Library           Sheet<br />
-
-MS1                     FEATHERM0BLE       FEATHER         adafruit          1<br />
-U$1                     Adafruit BNO055    BNO055          Adafruit_BNO055   1<br />
-MODESW                  OS102011MA1Q       OS102011MA1Q    adafruit          1<br />
-PSW                     OS102011MA1Q       OS102011MA1Q    adafruit          1<br />
-D1       CLEAR          LED-RGB-CACLEAR    LED-RGB-THRU    SparkFun-LED      1<br />
-R1       200            R-US_0207/7        0207/7          resistor          1<br />
-R2       200            R-US_0207/7        0207/7          resistor          1<br />
-R3       200            R-US_0207/7        0207/7          resistor          1<br />
-R4       10k            R-US_0207/7        0207/7          resistor          1<br />
-R5       10k            R-US_0207/7        0207/7          resistor          1<br />
-R6       10k            R-US_0207/7        0207/7          resistor          1<br />
-R7       10k            R-US_0207/7        0207/7          resistor          1<br />
-JP1                     PINHD-1X8          1X08            pinhead           1<br />
+CPST Design Project <br />
